8a22011514ee91765c5df50642eb9160b8f67ccc
lucas oliveira <lucas.meialua@outlook.com>
2023-03-26 10:52:13 -0300

    :truck: build: add router-view

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav id="nav">
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
